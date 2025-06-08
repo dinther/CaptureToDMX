76de126956e6d55b215b9228eccf608575824510
Paul van Dinther <vandinther@gmail.com>
2025-06-08 14:12:04 +1200

    Create index.js

---
+++ index.js
@@ -0,0 +1,117 @@
+const startElem = document.getElementById("startBtn");
+const videoElem = document.getElementById("video");
+const canvasElem = document.querySelector("canvas");
+var ctx;
+const stopElem = document.getElementById("endBtn");
+const dmxElem = document.getElementById("dmx");
+const blurSlider = document.getElementById("blur");
+const hueSlider = document.getElementById("hue");
+const saturationSlider = document.getElementById("saturation");
+const brightnessSlider = document.getElementById("brightness");
+const contrastSlider = document.getElementById("contrast");
+const fpsElem = document.getElementById("fps");
+
+var startTime;
+var frameCount = 0;
+blurSlider.addEventListener('input', (evt) => { ctx.filter = 'blur('+blurSlider.value+'px)' }, false);
+hueSlider.addEventListener('input', (evt) => { ctx.filter = 'hue-rotate('+hueSlider.value+'deg)' }, false);
+saturationSlider.addEventListener('input', (evt) => { ctx.filter = 'saturate('+saturationSlider.value+'%)' }, false);
+brightnessSlider.addEventListener('input', (evt) => { ctx.filter = 'brightness('+brightnessSlider.value+'%)' }, false);
+contrastSlider.addEventListener('input', (evt) => { ctx.filter = 'contrast('+contrastSlider.value+'%)' }, false);
+const samples = 24;
+const sampleLeftMargin =20;
+const sampleRightMargin=20;
+
+for (let i=0; i<samples; i++){
+  let sample = document.createElement('div');
+  let label = document.createElement('div');
+  label.innerText = i;
+  sample.appendChild(label);
+  sample.appendChild(document.createElement('div'));
+  sample.appendChild(document.createElement('div'));
+  sample.appendChild(document.createElement('div'));
+  sample.appendChild(document.createElement('div'));
+  dmxElem.appendChild(sample);
+}
+
+const displayMediaOptions = {
+  video: {
+    displaySurface: "window",
+    surfaceSwitching: "include",
+    selfBrowserSurface: "exclude",
+    cursor: "motion",
+  },
+  audio: false,
+};
+
+startElem.addEventListener('click', (evt) => { startCapture(); }, false);
+stopElem.addEventListener('click', (evt) => { stopCapture(); }, false);
+
+async function startCapture() {
+  try {
+    const stream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+    
+    const cropElem = document.querySelector("#crop");
+    const cropTarget = await CropTarget.fromElement(cropElem);
+    const [track] = stream.getVideoTracks();
+    // Crop video track
+    //await track.cropTo(cropTarget);
+    //await cropTarget.BrowserCaptureMediaStreamTrack.cropTo(cropTarget);
+    
+    
+    //const restrictionTarget = await RestrictionTarget.fromElement(demoElem);
+    //await track.restrictTo(restrictionTarget);
+    videoElem.srcObject = stream;
+    
+    // Wait for the video metadata to be loaded
+    videoElem.addEventListener('loadedmetadata', () => {
+      // Set the canvas dimensions based on the video dimensions
+      canvasElem.width = videoElem.videoWidth;
+      canvasElem.height = videoElem.videoHeight;
+      let vh = videoElem.videoHeight * 0.5;
+      // Draw the video stream on the canvas
+      function draw(timeStamp) {
+        if (ctx) {
+            ctx.drawImage(videoElem, 0, 0, canvasElem.width, canvasElem.height);
+            frameCount++;
+            if(timeStamp > startTime + 1000){
+                fpsElem.innerText = frameCount;
+                frameCount = 0;
+                startTime = timeStamp;
+            }
+            let data = ctx.getImageData(0, vh, videoElem.videoWidth-1, 1).data;
+            let step = Math.floor((videoElem.videoWidth - sampleLeftMargin - sampleRightMargin) / samples);
+            let channel = 1;
+            for (let i = sampleLeftMargin; i < videoElem.videoWidth; i += step) {
+            let r = data[(i * 4)];
+            let g = data[(i * 4) + 1];
+            let b = data[(i * 4) + 2];
+            let sample = dmxElem.children[channel - 1]; 
+            sample.children[1].innerText = r;
+            sample.children[2].innerText = g;
+            sample.children[3].innerText = b;
+            sample.style.backgroundColor = 'rgb('+r+','+g+','+b+')';
+            channel++;
+            if (channel>samples){ break; }
+          }
+        } else {
+            ctx = canvasElem.getContext('2d', {
+              willReadFrequently: true,
+            });
+        }
+        requestAnimationFrame(draw); // Continuously draw the stream
+      }
+      startTime = performance.now();
+      draw(startTime);
+    });
+    
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function stopCapture(evt) {
+  let tracks = videoElem.srcObject.getTracks();
+  tracks.forEach((track) => track.stop());
+  videoElem.srcObject = null;
+}
