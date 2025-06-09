a590b24a9d73e35cab83f8b0ac70e7b1b2262754
Paul <pvdinther@gmail.com>
2025-06-09 19:07:51 +1200

    Added line pos

--- index.js
+++ index.js
@@ -4,20 +4,31 @@ const canvasElem = document.querySelector("canvas");
 var ctx;
 const stopElem = document.getElementById("endBtn");
 const dmxElem = document.getElementById("dmx");
+const lineElem = document.getElementById("line");
 const blurSlider = document.getElementById("blur");
 const hueSlider = document.getElementById("hue");
 const saturationSlider = document.getElementById("saturation");
 const brightnessSlider = document.getElementById("brightness");
 const contrastSlider = document.getElementById("contrast");
+const linePosSlider = document.getElementById("linepos");
 const fpsElem = document.getElementById("fps");
 
 var startTime;
 var frameCount = 0;
+var vh = 0;
+
 blurSlider.addEventListener('input', (evt) => { ctx.filter = 'blur('+blurSlider.value+'px)' }, false);
 hueSlider.addEventListener('input', (evt) => { ctx.filter = 'hue-rotate('+hueSlider.value+'deg)' }, false);
 saturationSlider.addEventListener('input', (evt) => { ctx.filter = 'saturate('+saturationSlider.value+'%)' }, false);
 brightnessSlider.addEventListener('input', (evt) => { ctx.filter = 'brightness('+brightnessSlider.value+'%)' }, false);
 contrastSlider.addEventListener('input', (evt) => { ctx.filter = 'contrast('+contrastSlider.value+'%)' }, false);
+linePosSlider.addEventListener('input', (evt) => {
+  lineElem.style.top = linePosSlider.value+'%';
+  vh = videoElem.videoHeight * linePosSlider.value * 0.01;
+  //linePos = linePosSlider.value * 0.01;
+
+}, false);
+
 const samples = 24;
 const sampleLeftMargin =20;
 const sampleRightMargin=20;
@@ -25,7 +36,7 @@ const sampleRightMargin=20;
 for (let i=0; i<samples; i++){
   let sample = document.createElement('div');
   let label = document.createElement('div');
-  label.innerText = i;
+  label.innerText = i*3;
   sample.appendChild(label);
   sample.appendChild(document.createElement('div'));
   sample.appendChild(document.createElement('div'));
@@ -68,7 +79,8 @@ async function startCapture() {
       // Set the canvas dimensions based on the video dimensions
       canvasElem.width = videoElem.videoWidth;
       canvasElem.height = videoElem.videoHeight;
-      let vh = videoElem.videoHeight * 0.5;
+      vh = videoElem.videoHeight * 0.5;
+      
       // Draw the video stream on the canvas
       function draw(timeStamp) {
         if (ctx) {
